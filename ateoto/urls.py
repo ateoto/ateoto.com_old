5c4f079dbaa48e5ba1faf9687d3290ff15c6fbe5
Matthew McCants <mattmccants@gmail.com>
2012-08-14 03:45:41 -0500

    Fix urls.

--- ateoto/urls.py
+++ ateoto/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name = 'splash.html')),
-    url(r'^dayboard/', include(ateoto_dayboard.urls)),
+    url(r'^dayboard/', include('ateoto_dayboard.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
