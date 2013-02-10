15eb30f91012f96a6e1c3014aba785799efcec8b
Matthew McCants <mattmccants@gmail.com>
2013-02-10 00:10:03 -0600

    Readying ateoto.com for deploy of django-character-builder.

--- ateoto/urls.py
+++ ateoto/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name = 'splash.html')),
+    url(r'^$', TemplateView.as_view(template_name='splash.html')),
     url(r'^dayboard/', include('ateoto_dayboard.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^DnD/', include('character_builder.urls')),
