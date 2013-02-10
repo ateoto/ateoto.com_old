f2e75625316edcc5a8e5ff816f4d20caf3098f97
Matthew McCants <mattmccants@gmail.com>
2013-02-10 01:55:43 -0600

    Added Grappelli to urls.py.

--- ateoto/urls.py
+++ ateoto/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='splash.html')),
     url(r'^dayboard/', include('ateoto_dayboard.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^DnD/', include('character_builder.urls')),
 )
