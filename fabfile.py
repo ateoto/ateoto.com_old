c9a77644fd24d0294018aa205f3fc04acf0a0f98
Matthew McCants <mattmccants@gmail.com>
2013-02-16 19:48:46 -0600

    Some PEP8 changes to fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,24 +1,26 @@
-import os
 from fabric.api import run, cd, env, put, prefix, sudo
-from fabric.contrib import files
 
 env.deploy_dir = '/home/ubuntu/ateoto.com'
 env.default_deploy_ref = 'origin/master'
 env.virtualenv = 'ateoto.com'
+env.hosts = ['ubuntu@ateoto.com']
+
 
 def deploy():
     update_code()
     install_requirements()
     collect_static()
-    apache('reload')    
+    apache('reload')
 
-def update_code(ref = None):
+
+def update_code(ref=None):
     ref = ref or env.default_deploy_ref
 
     with cd(env.deploy_dir):
         run('git pull')
         put('ateoto/settings/secrets.json', 'ateoto/settings/secrets.json')
-    
+
+
 def install_requirements():
     with cd(env.deploy_dir):
         with prefix('workon %s' % (env.virtualenv)):
@@ -26,13 +28,15 @@ def install_requirements():
             run('python manage.py migrate --settings=ateoto.settings')
             run('python manage.py syncdb --settings=ateoto.settings')
 
-def collect_static(clear = False):
+
+def collect_static(clear=False):
     clear_cmd = ''
-    if clear: clear_cmd = '--clear'
+    if clear:
+        clear_cmd = '--clear'
     with cd(env.deploy_dir):
         with prefix('workon %s' % (env.virtualenv)):
             run('python manage.py collectstatic --noinput %s --settings=ateoto.settings' % (clear_cmd))
 
+
 def apache(cmd):
     sudo('invoke-rc.d apache2 %s' % cmd)
-
